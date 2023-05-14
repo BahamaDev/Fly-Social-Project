27223b42de7102c95bf5085b190d3ce0102bccd7
BahamaDev <41267607+BahamaDev@users.noreply.github.com>
2023-05-14 05:56:55 -0400

    Loading spinner completed. Search queries being built.

--- flysocial_front/flysocial-app/src/components/Feed.jsx
+++ flysocial_front/flysocial-app/src/components/Feed.jsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { client } from "../client";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
+import { searchQuery } from "../utils/data";
 
 const Feed = () => {
+  const [loading, setLoading] = useState(false);
+  const { categoryId } = useParams();
+  const [pins, setPins] = useState(null);
+
+  useEffect(() => {
+    // If there is a category Id fetch the data using that as the argument and query parameter.
+    if (categoryId) {
+      const query = searchQuery(categoryId);
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+    }
+  }, [categoryId]);
+  if (loading) return <Spinner message='Updating your Feed!' />;
   return <div>Feed</div>;
 };
 
